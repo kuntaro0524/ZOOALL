c405e737504d4a3702bf36218ed81eac60c104a6
kuntaro <kuntaro0524@gmail.com>
2024-05-17 11:29:41 +0900

    URL was modified.

--- TestScripts/echa_test01.py
+++ TestScripts/echa_test01.py
@@ -14,24 +14,25 @@ import pandas as pd
 
 def get_access_token():
     global access_token, token_expiry
-    print("EEEEEEEEEEEEEEEEEE")
     print(LOGIN_URL, USERNAME, PASSWORD)
     response = requests.post(LOGIN_URL, data={"username": USERNAME, "password": PASSWORD})
+    print(response.status_code)
     if response.status_code == 200:
         token_data = response.json()
-        print(token_data)
         access_token = token_data['access_token']
         # トークンが作成された時刻は 'last_login' に格納されている
         # この時間に１時間を足した時間がトークンの有効期限
         # format '2024-04-25T07:21:34.153331+09:00'
         last_login = datetime.strptime(token_data['user']['last_login'], "%Y-%m-%dT%H:%M:%S.%f%z")
-        expiry = last_login + timedelta(hours=1)
-        print("Expired" , expiry)
+        token_expiry = last_login + timedelta(hours=1)
+        print("Expired" , token_expiry)
 
     else:
+        print("失敗した!")
         raise Exception("Failed to login")
 
 def make_authenticated_request():
+    print("make_authenticated_request")
     global access_token, token_expiry
     # トークンの有効期限が切れているか確認
     if token_expiry is None or datetime.now() >= token_expiry:
@@ -51,37 +52,24 @@ def make_authenticated_request():
     for i in range(len(df)):
         # 'measure_id' を取得
         meas_id=df.iloc[i]['measure_id']
-        target_url = f"{API_URL}/parameter_measure/"
-        response = requests.get(target_url, headers=headers, params={"measure_id":meas_id})
-        print(response.json())
-        each_df = pd.DataFrame(response.json())
-        print(each_df.columns)
-        dicts = {}
-        for j in range(len(each_df)):
-        # この each_df に含まれている１行のデータの 'parameter_name' と 'value' で dictionaryを作成
-            dicts[each_df.iloc[j]['parameter_name']] = each_df.iloc[j]['value']
-
-        # dicts を Series に変換
-        each_pin_df = pd.Series(dicts)
+        print("Measure ID: ", meas_id)
+        response_json = requests.get("https://dcha-spx.spring8.or.jp/api1.0.2/parameter_measure/get_list", headers=headers, params={"measure_id":meas_id})
+        print("######")
+        print(response_json)
+        print("######")
+        # each_pin_df = pd.Series(dicts)
         # print(each_pin_df)
-        data_list.append(each_pin_df)
+        # data_list.append(each_pin_df)
 
     # data_list からpandas DataFrameを作成
-    conds_df = pd.DataFrame(data_list)
+    # conds_df = pd.DataFrame(data_list)
     # print(conds_df)
 
     return conds_df
 
 # データを取得する関数を使用
-start_time = datetime.now()
 conds_df = make_authenticated_request()
 print("#####################")
 for each_cond in conds_df.iterrows():
     print(each_cond)
 print("#####################")
-
-end_time = datetime.now()
-# 消費時間を計算 (sec)
-consumed_time = end_time - start_time
-print("Consumed Time: ", consumed_time)
-
