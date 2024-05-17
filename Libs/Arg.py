8ef68a8e4b203d1b6225477f2df3797a98b6811d
kuntaro <kuntaro0524@gmail.com>
2024-05-17 11:32:09 +0900

    Mac Okkotonushi -> okkotonushi とかわけわからんことがおきた git config core.ignorecase false としてコミットしたらこうなった

--- Libs/Arg.py
+++ Libs/Arg.py
@@ -1,17 +1,34 @@
-from argparse import ArgumentParser
+import argparse    
 
-def get_option(batch, epoch):
-    argparser = ArgumentParser()
-    #argparser.add_argument('-o', '--offline', type = int, default=batch, help = 'Specify size of batch')
-    argparser.add_argument('-db', '--database', type = char, default=batch, help = 'Specify size of batch', action="gogogo")
-    #argparser.add_argument('-c', '--batch', type = int, default=batch, help = 'Specify size of batch')
+parser = argparse.ArgumentParser(description='getget')
 
-    return argparser.parse_args()
+#parser.add_argument('arg1', help='arg1 explanation')
+#parser.add_argument('arg2', help='foooo')
+parser.add_argument('--csv',help='1st CSV file') 
+parser.add_argument('--csv2', help='2nd CSV file')
+parser.add_argument('--csv3', help='3rd CSV file')
+parser.add_argument('--dbfile', help='definition of the database file')
 
+args = parser.parse_args()   
 
-#args=get_option(BATCH_SIZE, EPOCHS)
-parser = ArgumentParser()
-parser.add_argument('-db', '--database', type = str, default="zoo.db", help = 'Specify size of batch')
-parser.add_argument('--offline', type = bool, default=False, help = 'Specify size of batch')
-args = parser.parse_args()
-print(args)
+csvlist = []
+csv_flag = False
+
+if args.csv != None:
+    csv_flag = True
+    csv_name = args.csv
+    csvlist.append(csv_name)
+
+if args.csv2 != None:
+    csv2_name = args.csv2
+    csvlist.append(csv2_name)
+
+if args.csv3 != None:
+    csv3_name = args.csv3
+    csvlist.append(csv3_name)
+
+if csv_flag == True:
+    for csvfile in csvlist:
+        print(csvfile)
+else:
+    print("DBFILE=", args.dbfile)
