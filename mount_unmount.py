2864fb8f47bca3962d5d48c1107efa4ca78ddfd3
Kunio Hirata <kuntaro0524@gmail.com>
2024-10-09 17:30:44 +0900

    kobayashi script was coded

--- mount_unmount.py
+++ mount_unmount.py
@@ -3,9 +3,9 @@ import time
 import numpy as np
 import socket
 
-sys.path.append("/isilon/BL32XU/BLsoft/PPPP/10.Zoo/Libs/")
+#sys.path.append("/isilon/BL32XU/BLsoft/PPPP/10.Zoo/Libs/")
 from MyException import *
-import MXserver
+#import MXserver
 import Zoo
 import Date
 import logging
@@ -28,7 +28,8 @@ if __name__ == "__main__":
 
     num = 0
     while num < 5:
-        for trayid in ["1", "2", "3", "4", "5", "6", "7", "8"]:
+        #for trayid in ["1", "2", "3", "4", "5", "6", "7", "8"]:
+        for trayid in ["CPS1125"]:
             for pinid in range(1, 17):
                 logger.info("Tray %s - Pin %02d mount started." % (trayid, pinid))
                 try:
@@ -39,8 +40,7 @@ if __name__ == "__main__":
                     sys.exit(1)
                 logger.info("Tray %s - Pin %02d mount finished." % (trayid, pinid))
                 logger.info("Waiting for 2 minutes from now")
-
-                time.sleep(120)
+                time.sleep(60)
                 logger.info("Tray %s - Pin %02d dismount started." % (trayid, pinid))
                 zoo.dismountCurrentPin()
                 logger.info("%s - %02d dismount finished." % (trayid, pinid))
