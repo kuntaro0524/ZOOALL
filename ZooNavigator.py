b7c024074189105b89eac23982a7053db5b52474
Kunio Hirata <kuntaro0524@gmail.com>
2025-01-22 12:32:40 +0900

    Measure flux flag -> recovered

--- ZooNavigator.py
+++ ZooNavigator.py
@@ -117,7 +117,7 @@ class ZooNavigator():
         self.meas_flux_list = []
         self.meas_wavelength_list = []
 
-        self.needMeasureFlux = False  # test at 2019/06/18 at BL45XU
+        self.needMeasureFlux = True  # test at 2019/06/18 at BL45XU
 
         # If BSS can change beamsize via command
         self.doesBSSchangeBeamsize = True
